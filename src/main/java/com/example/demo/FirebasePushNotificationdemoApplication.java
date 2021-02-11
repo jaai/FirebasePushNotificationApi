3562861dcbe5783e6819b32061140a660441fc05
jai Kasar <jaikasar@gmail.com>
2021-02-11 10:58:44 +0530

    first commit

---
+++ src/main/java/com/example/demo/FirebasePushNotificationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebasePushNotificationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebasePushNotificationdemoApplication.class, args);
+	}
+
+}
